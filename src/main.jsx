ec290055895bb6c7b680accbaf66d8235af55d3d
devilkiller-ag <ashmitgupta.official@gmail.com>
2024-04-15 10:54:32 +0530

    added vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Analytics } from '@vercel/analytics/react';
 import './index.css';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+  <>
+    <App />
+    <Analytics />
+  </>
 );
